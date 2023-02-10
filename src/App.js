709a5023e04dd1a18a0ddb6bae44111aa29e0a67
ssonggary <84754325+ssonggary@users.noreply.github.com>
2023-02-10 16:37:32 +0900

    redux, axios 셋팅

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 //component
 import { Gnb, Main, Signup, Signin, TodoList } from "./components";
 
--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Button, Form, Container } from "react-bootstrap";
 
 const Signin = () => {
-  return <div>로그인</div>;
+  return (
+    <>
+      <Container className="position-absolute top-50 start-50 translate-middle w-50">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group> */}
+          <div>
+            <Button variant="primary" type="submit" className="me-2">
+              로그인
+            </Button>
+            <Button type="submit">회원가입</Button>
+          </div>
+        </Form>
+      </Container>
+    </>
+  );
 };
 
 export default Signin;
